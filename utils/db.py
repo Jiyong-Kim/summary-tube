363ae4e2380e67783f553255852b66989f79d8be
Jiyong <125526982+kimjiyong3012@users.noreply.github.com>
2026-03-17 20:41:23 +1300

    로그추가

--- utils/db.py
+++ utils/db.py
@@ -13,6 +13,7 @@ def get_supabase() -> Client:
     if _supabase is None:
         url = os.getenv("SUPABASE_URL")
         key = os.getenv("SUPABASE_KEY")
+        print(f"url : [{url}], key : [{key}]")
         if not url or not key:
             raise ValueError("SUPABASE_URL 또는 SUPABASE_KEY가 설정되지 않았습니다!")
         _supabase = create_client(url, key)
